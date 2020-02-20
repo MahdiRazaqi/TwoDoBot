9d18dfb4c395281ab8670b82b56062f3f413fe7b
MahdiRazaqi <mahdirazaqi77@gmail.com>
2020-02-20 23:25:42 +0330

    Added delete one task from jsonDB

--- jsondb/request.go
+++ jsondb/request.go
@@ -17,3 +17,34 @@ func AddTask(task Task) {
 
 	WriteDB(path, data)
 }
+
+// DeleteTask to To-do
+func DeleteTask(task Task) {
+	id := strconv.Itoa(task.User.ID)
+	path := DBPath + id + ".json"
+
+	data := TodoList{}
+	if HasExist(path) {
+		data = ReadDB(path)
+	}
+
+	var cardList *[]Task
+	switch task.Status {
+	case "todo":
+		cardList = &data.Todo
+	case "doing":
+		cardList = &data.Doing
+	case "done":
+		cardList = &data.Done
+	}
+
+	for i, t := range *cardList {
+		if t.ID == task.ID {
+
+			(*cardList)[i] = (*cardList)[len(*cardList)-1]
+			*cardList = (*cardList)[:len(*cardList)-1]
+		}
+	}
+
+	WriteDB(path, data)
+}
