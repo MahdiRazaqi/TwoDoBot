8ccc98f1a3f83cb18fdd60d892fe362eae79cd71
MahdiRazaqi <mahdirazaqi77@gmail.com>
2020-02-28 21:22:45 +0330

    Added select func for get data from jsonDB

--- jsondb/request.go
+++ jsondb/request.go
@@ -1,6 +1,7 @@
 package jsondb
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -18,7 +19,7 @@ func AddTask(task Task) {
 	WriteDB(path, data)
 }
 
-// DeleteTask to To-do
+// DeleteTask from jsonDB
 func DeleteTask(task Task) {
 	id := strconv.Itoa(task.User.ID)
 	path := DBPath + id + ".json"
@@ -40,11 +41,34 @@ func DeleteTask(task Task) {
 
 	for i, t := range *cardList {
 		if t.ID == task.ID {
-
 			(*cardList)[i] = (*cardList)[len(*cardList)-1]
 			*cardList = (*cardList)[:len(*cardList)-1]
 		}
 	}
 
 	WriteDB(path, data)
+}
+
+// SelectTask from DB
+func SelectTask(user User, list string) []Task {
+	id := strconv.Itoa(user.ID)
+	path := DBPath + id + ".json"
+
+	data := TodoList{}
+	if HasExist(path) {
+		data = ReadDB(path)
+	}
+
+	var cardList *[]Task
+	switch list {
+	case "todo":
+		cardList = &data.Todo
+	case "doing":
+		cardList = &data.Doing
+	case "done":
+		cardList = &data.Done
+	}
+
+	fmt.Println(cardList)
+	return *cardList
 }
